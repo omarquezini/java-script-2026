46706ebd64a495d62530a13649ed9c6218e67d6b
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2026-02-09 13:06:16 +0000

    java escript.

---
+++ calculaMedia.js
@@ -0,0 +1,20 @@
+let nota1 = 5;
+
+let nota2 = 3;
+
+let nota3 = 5;
+
+let nota4 = 7;
+
+let nota5 = 5;
+
+let nota6 = 9;
+
+
+
+const nomeAluno = "Wellberton";
+
+let media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6) / 6;
+
+console.log ("O aluno: " + nomeAluno);
+console.log ("Tem a media: " + media);

76fe9a022fcac25802db1368a500460297e1668d
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2026-02-09 17:36:37 +0000

    fazendo besteirinhas.

--- calculaMedia.js
+++ calculaMedia.js
@@ -6,15 +6,21 @@ let nota3 = 5;
 
 let nota4 = 7;
 
-let nota5 = 5;
-
-let nota6 = 9;
-
-
 
 const nomeAluno = "Wellberton";
 
-let media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6) / 6;
+let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+if (media >= 7){
+    console.log("O aluno"+ nomeAluno + "está aprovado, agora caia fora seu misereval");
+}
+if (media >= 5 && media < 7){
+    console.log("mediocre, vai pro inferno ( se liga ).");
+
+}
+if (media < 5){
+    console.log("**suspiro**...como, COMO! você pode ser tão burro.");
+}
 
 console.log ("O aluno: " + nomeAluno);
 console.log ("Tem a media: " + media);

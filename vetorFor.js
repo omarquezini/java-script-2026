cb0fc7190e903b661f180352830bf30420136350
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2026-03-25 13:07:14 +0000

    muita coisa.

--- vetorFor.js
+++ vetorFor.js
@@ -1,18 +1,17 @@
 const readline = require("readline-sync");
 
-
-
-const notas = [7.5, 8.0, 6.0, 9.5, 5.0];
-
+const notas = [];
 let soma = 0;
 
-for (let i = 0; i < notas.length; i++) {
-    soma = soma + notas[i];
+let numeros = parseInt(readline.question("Quantas materias você tem? "), 10); // converter para número
+
+for (let i = 0; i < numeros; i++) {
+    let nota = parseFloat(readline.question("Digite sua nota: ")); // converter para número decimal
+    notas.push(nota);
+    soma += nota; // somando já os números
 }
 
 const media = soma / notas.length;
-
-
 
 console.log(`A soma das notas é: ${soma.toFixed(2)}`);
 console.log(`A média das notas é: ${media.toFixed(2)}`);
